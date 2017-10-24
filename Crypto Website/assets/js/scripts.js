a8fd84b4c9d7be142e1169aec1bcc1ae2fe83ad5
Unknown <kristiaanpretorius@hotmail.com>
2017-10-25 00:40:54 +0200

    user verification
    fixed

--- Crypto Website/assets/js/scripts.js
+++ Crypto Website/assets/js/scripts.js
@@ -1,15 +1,15 @@
 
 jQuery(document).ready(function() {
-    
+
     /*
         Login form validation
     */
     $('.login-form input[type="text"], .login-form input[type="password"], .login-form textarea').on('focus', function() {
     	$(this).removeClass('input-error');
     });
-    
+
     $('.login-form').on('submit', function(e) {
-    	
+
     	$(this).find('input[type="text"], input[type="password"], textarea').each(function(){
     		if( $(this).val() == "" ) {
     			e.preventDefault();
@@ -19,18 +19,18 @@ jQuery(document).ready(function() {
     			$(this).removeClass('input-error');
     		}
     	});
-    	
+
     });
-    
+
     /*
         Registration form validation
     */
     $('.registration-form input[type="text"], .registration-form textarea').on('focus', function() {
     	$(this).removeClass('input-error');
     });
-    
+
     $('.registration-form').on('submit', function(e) {
-    	
+
     	$(this).find('input[type="text"], textarea').each(function(){
     		if( $(this).val() == "" ) {
     			e.preventDefault();
@@ -40,8 +40,23 @@ jQuery(document).ready(function() {
     			$(this).removeClass('input-error');
     		}
     	});
-    	
+
     });
-    
-    
+    var email = $('#form-username').val();
+		var password = $('#form-password').val();
+
+		var url = 'http://cryptoflo-env.eu-west-1.elasticbeanstalk.com/api/users?email=' + email + '&password=' + password
+
+		$.ajax({
+			url: url,
+			type: "POST",
+			crossDomain: true,
+			success:function(result){
+				alert(JSON.stringify(result));
+			},
+			error:function(xhr,status,error){
+				alert(status);
+			}
+		});
+
 });
